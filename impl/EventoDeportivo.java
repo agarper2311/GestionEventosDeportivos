361731ee3e468eabcb0f18a80ffd13ecbf6f7897
dsanvaz2103 <dsanvaz2103@g.educaand.es>
2024-03-01 14:42:15 +0100

    Actualizacion proyecto 01/03/24

---
+++ impl/EventoDeportivo.java
@@ -0,0 +1,14 @@
+package com.es.programacion.Tema6.proyectoGestionEventoDeportivo.impl;
+
+import com.es.programacion.Tema6.proyectoGestionEventoDeportivo.api.Ganador;
+
+import java.util.ArrayList;
+
+public class EventoDeportivo implements Ganador {
+
+    //Constructor
+    @Override
+    public Participante obtenerGanador(ArrayList<Participante> participantes) {
+        return null;
+    }
+}
---
+++ impl/ParticipanteCarrera.java
@@ -0,0 +1,41 @@
+package com.es.programacion.Tema6.proyectoGestionEventoDeportivo.impl;
+
+import java.util.ArrayList;
+
+public class ParticipanteCarrera extends Participante {
+
+    //Atributos
+    private double tiempo;
+
+    // Constructor
+    public ParticipanteCarrera(String dni, String nombre, String apellidos, int edad, double tiempo) {
+        super(dni, nombre, apellidos, edad);
+        this.tiempo = tiempo;
+    }
+
+    // Getter y setter para el tiempo
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    // Método toString
+    @Override
+    public String toString() {
+        return "ParticipanteCarrera{" +
+                "dni='" + getDni() + '\'' +
+                ", nombre='" + getNombre() + '\'' +
+                ", apellidos='" + getApellidos() + '\'' +
+                ", edad=" + getEdad() +
+                ", tiempo=" + tiempo +
+                '}';
+    }
+
+
+
+
+
+}
