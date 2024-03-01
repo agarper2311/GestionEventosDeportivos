5e0d33181e14b43bcb4daa1f99fdc8d081ce40cb
Ángel <agarper2311@g.educaand.es>
2024-03-01 14:58:29 +0100

    cambios src angel

---
+++ src/impl/EventoDeportivo.java
@@ -0,0 +1,14 @@
+package impl;
+
+import com.es.programacion.Tema6.proyectoGestionEventoDeportivo.api.Ganador;
+
+import java.util.ArrayList;
+
+public class EventoDeportivo implements Ganador {
+
+    //Constructor
+    @Override
+    public Participante obtenerGanador(ArrayList<Participante> participantes) {
+        return null;
+    }
+}
---
+++ src/impl/Participante.java
@@ -0,0 +1,79 @@
+package impl;
+
+public class Participante {
+
+    //Atributos de clase
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    /**
+     * Constructor que recibe por parametros:
+     * @param dni
+     * @param nombre
+     * @param apellidos
+     * @param edad
+     */
+    public Participante(String dni, String nombre, String apellidos, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    //Getters y Setters
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    //Metodo toString
+
+    @Override
+    public String toString() {
+        return "Participante{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+
+    // Método equals
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Participante that = (Participante) obj;
+        return dni.equals(that.dni);
+    }
+}
